b886f07a599cf4b25355fb609928ed2dc4a56254
panzerkraftwerg <drgak_isp_2020d@edu.kait20.ru>
2023-03-20 17:00:40 +0300

    first commit

---
+++ Lab6Fomina/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Lab6Fomina.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab6Fomina.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        private Users user = new Users();
+        public RegisterPage()
+        {
+            InitializeComponent();
+
+            DataContext = user;
+        }
+
+        private void RegisterBT_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dfominaEntities.GetContext().Users.FirstOrDefault(x => x.Email == user.Email) == null)
+                {
+                    user.IDSecretQuestion = 1;
+
+                    MessageBox.Show(Manager.PrintProperties(user));
+
+                    dfominaEntities.GetContext().Users.Add(user);
+                    dfominaEntities.GetContext().SaveChanges();
+
+                    MessageBox.Show("Успех!", "Успех!");
+
+                    Manager.MainFrame.Navigate(new LoginPage());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\n" + ex.ToString(), "Ошибка!");
+            }
+        }
+    }
+}
---
+++ Lab6Fomina/Utilities/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+public static class Manager
+{
+    public static Frame MainFrame { get; set; }
+
+    public static string PrintProperties(Object obj)
+    {
+        string res = "";
+        foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+        {
+            string name = descriptor.Name;
+            object value = descriptor.GetValue(obj);
+            res += $"\n{name}={value}";
+        }
+        return res;
+    }
+
+
+
+    //private static dfominaEntities context;
+    //public static dfominaEntities GetContext()
+    //{
+    //    if (context == null)
+    //        context = new dfominaEntities();
+
+    //    return context;
+    //}
+}
